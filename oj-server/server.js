f87bf2cb7af283f5ec754b967016df4df96e6b98
ganqianjun <ganqianjun@outlook.com>
2017-05-11 00:57:38 -0700

    COJ Server :  Integrates with MongoDB

--- oj-server/server.js
+++ oj-server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var restRouter = require('./routes/rest.js');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://admin:admin@ds133249.mlab.com:33249/coj-ganqianjun');
 
 app.use('/api/v1', restRouter);
 
