e2de469a29486262d874796b80add14f6f1953c8
ganqianjun <ganqianjun@outlook.com>
2017-05-11 00:00:37 -0700

    COJ Server : Add RESTful API and problemService - getProblems, getProblem, addProblem

--- oj-server/server.js
+++ oj-server/server.js
@@ -1,10 +1,9 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var restRouter = require('./routes/rest.js');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use('/api/v1', restRouter);
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
