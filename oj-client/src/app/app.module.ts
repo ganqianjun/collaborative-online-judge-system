c9e400d10eabe9603d9fab9cf7aa56395f9a5206
ganqianjun <ganqianjun@outlook.com>
2017-05-13 00:15:12 -0700

    COJ Client : Fix bug - No provider for authGuard (the import order in app.module.ts matters

--- oj-client/src/app/app.module.ts
+++ oj-client/src/app/app.module.ts
@@ -3,17 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ProblemListComponent } from './components/problem-list/problem-list.component';
-import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
-import { NewProblemComponent } from './components/new-problem/new-problem.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { routing } from './app.routes';
 
 import { DataService } from './services/data.service';
 import { AuthService } from './services/auth.service';
 import { AuthGuardService } from './services/auth-guard.service';
 
-import { routing } from './app.routes';
+import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
