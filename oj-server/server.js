0fdf9d63460611b730d7fd11702034cadb0e1e86
ganqianjun <ganqianjun@outlook.com>
2017-05-14 23:00:13 -0700

    COJ Server : Add websocket.io

--- oj-server/server.js
+++ oj-server/server.js
@@ -18,8 +18,32 @@ app.use( function(req, res, next) {
   res.sendFile( 'index.html', {
     root: path.join( __dirname, '../production' )
   })
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
 });
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+
+var http = require('http');
+var socketIO = require('socket.io');
+var io = socketIO();
+var editorSocketService = require('./services/editorSocketService')(io);
+
+// create http server and add io to it
+var server = http.createServer(app);
+io.attach(server);
+server.listen(3000);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+  throw error;
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
---
+++ oj-server/services/editorSocketService.js
@@ -0,0 +1,8 @@
+module.exports = function(io) {
+  io.on('connection', (socket) => {
+    console.log(socket);
+    var message = socket.handshake.query['message'];
+    console.log(message);
+    io.to(socket.id).emit('message', 'received the message (server)');
+  })
+}
