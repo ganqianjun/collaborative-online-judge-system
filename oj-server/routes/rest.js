30564ba3a90f23803446ab35f169a4cdee0edc57
ganqianjun <ganqianjun@outlook.com>
2017-05-21 23:05:26 -0700

    COJ Server : Add '/api/v1/builder' under restRouter

--- oj-server/routes/rest.js
+++ oj-server/routes/rest.js
@@ -5,17 +5,20 @@ var jsonParser = bodyParser.json();
 
 var problemService = require('../services/problemService');
 
+// get: /api/v1/problems
 router.get('/problems', function(req, res) {
   problemService.getProblems()
     .then( problems => res.json(problems) );
 });
 
+// get: /api/v1/problems/:id
 router.get('/problems/:id', function(req, res) {
   var id = req.params.id;
   problemService.getProblem(+id)
     .then( problem => res.json(problem) );
 });
 
+// post: /api/v1/problems
 router.post('/problems', jsonParser, function(req, res) {
   problemService.addProblem(req.body)
     .then(problem => {
@@ -24,6 +27,16 @@ router.post('/problems', jsonParser, function(req, res) {
     error => {
       res.status(400).send('Problem already exists');
     });
+});
+
+// post: /api/v1/builder
+router.post('/builder', jsonParser, function(req, res) {
+  const userCodes = req.body.userCodes;
+  const language = req.body.language;
+  console.log('rest.js : codes - ' + userCodes + ' ; language - ' + language);
+  res.json({
+    'text': 'hello from rest.js'
+  });
 });
 
 module.exports = router;
